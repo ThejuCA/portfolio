486b2541c1f0d832605be34577a75ba6fd3b1587
Chetan Raju <thejuanand1989@gmail.com>
2016-05-13 07:13:19 +1000

    Update
    Contact

--- contact.php
+++ contact.php
@@ -1,27 +1,34 @@
-<?PHP
-
-
-$host="sql210.lockernerd.co.uk";
-$user="lnw_16618010";
-$password="thejunan";
-
-$dbname = "lnw_16618010_theju";
-
-// Create connection
-$conn = new mysqli($host, $user, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "INSERT INTO clients (name, email,phone)
-VALUES ('Doe', 'john@example.com',902399)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
+<?php
+$action=$_REQUEST['action'];
+if ($action=="")    /* display the contact form */
+    {
+    ?>
+    <form  action="" method="POST" enctype="multipart/form-data">
+    <input type="hidden" name="action" value="submit">
+    Your name:<br>
+    <input name="name" type="text" value="" size="30"/><br>
+    Your email:<br>
+    <input name="email" type="text" value="" size="30"/><br>
+    Your message:<br>
+    <textarea name="message" rows="7" cols="30"></textarea><br>
+    <input type="submit" value="Send email"/>
+    </form>
+    <?php
+    }
+else                /* send the submitted data */
+    {
+    $name=$_REQUEST['name'];
+    $email=$_REQUEST['email'];
+    $message=$_REQUEST['message'];
+    if (($name=="")||($email=="")||($message==""))
+        {
+        echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+        }
+    else{
+        $from="From: $name<$email>\r\nReturn-path: $email";
+        $subject="Message sent using your contact form";
+        mail("thejuanand1989@gmail.com", $subject, $message, $from);
+        echo "Email sent!";
+        }
+    }
 ?>
